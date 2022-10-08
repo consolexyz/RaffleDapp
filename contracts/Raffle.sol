3f9f19bb2b105dd56ca2cf7c74d5b515adccbde4
consolexyz <mosesobike@gmail.com>
2022-10-08 08:23:05 +0100

    few solidity code updates

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,1 +1,40 @@
-// 
+// SPDX-Licenese-Identifier : MIT 
+
+pragma solidity ^0.8.9;
+
+error Raffle__NotEnoughETHEntered();   
+
+contract Raffle {
+    /* State Variables */
+
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+
+    event RaffleEnter( address indexed  player);
+    
+
+    constructor(uint256 entranceFee ){
+        i_entranceFee = entranceFee;
+
+    }
+    
+    function enterRaflle() public payable {
+
+        if(msg.value < i_entranceFee){
+            revert Raffle__NotEnoughETHEntered();  
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    // function pickRandomWinner() {}
+
+    function getEntranceFee() public view returns(uint256){
+        return i_entranceFee;
+    }
+    function getPlayers(uint256 index) public view returns(address){
+        return s_players[index];
+    }
+}
