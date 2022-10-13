baab23d22cb2d420c8ce6973ad3da6291651809a
consolexyz <mosesobike@gmail.com>
2022-10-13 03:50:02 +0100

    finished contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,34 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+
+const GOERIL_RPC_URL = process.env.GOERIL_RPC_URL;
+const PRIVATE_KEY= process.env.GOERLI_PRIVATE_KEY;
+
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat:{
+      chainId: 31337,
+      blockConfirmations:1,
+    },
+    goeril:{
+      chainId:5,
+      blockConfirmations:6,
+      url: GOERIL_RPC_URL,
+      accounts: PRIVATE_KEY,
+    }
+  },
   solidity: "0.8.17",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    }
+  }
 };
---
+++ helper-hardhat-config.js
@@ -0,0 +1,15 @@
+const networkConfig = {
+    5: {
+        name: "rinkeby",
+        vrfCoordinatorV2:"0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D"
+    }
+}
+
+
+const developmentChains = ["hardhat" , "localhost"]
+
+
+module.exports = {
+    networkConfig,
+    developmentChains
+}
