e6e442f31d25e688a22975d8c152a789006f6c55
consolexyz <mosesobike@gmail.com>
2022-10-17 03:23:36 +0100

    fixed server issues

---
+++ dapp/components/Headers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Header() {
+    return(
+        <div className="header">
+            Header
+            <h1>Connect button</h1>
+        </div>
+    )
+}
