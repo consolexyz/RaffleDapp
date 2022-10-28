eb2e99a6e5466efb8fcf85bab11ea7e9017cd512
consolexyz <mosesobike@gmail.com>
2022-10-28 05:16:50 +0100

    fixed modal issues

--- dapp/components/Main.jsx
+++ dapp/components/Main.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function Main (){
     return(
         <div className="raffle">
-            <div className="enterRaffle">
                 <button className="custom-button">Enter Raffle</button>
-            </div>
         </div>
     )
 }
